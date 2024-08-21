90a0bc33a4f7e42bc2435a3dc54b0a89e4c392d5
bengu <bengukaratas07@gmail.com>
2024-08-20 21:31:10 -0400

    Java video 30

---
+++ src/V030_toStringMethod.java
@@ -0,0 +1,16 @@
+public class V030_toStringMethod {
+
+    public static void main(String[] args) {
+
+        // toString() = special method that all objects inherit,
+        //              that returns a string that "textually represents" am object
+        //              Can be used both implicitly and explicitly
+
+        Car car = new Car();
+
+
+        //System.out.println(car.toString());
+        System.out.println(car);
+
+    }
+}
