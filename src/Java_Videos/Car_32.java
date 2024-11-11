f0d706d63a5f9a4a63e7af8d51d4fb5d6d4c1c70
bengu <bengukaratas07@gmail.com>
2024-11-11 15:45:28 -0500

    Java video 32-33

---
+++ src/Java_Videos/Car_32.java
@@ -0,0 +1,13 @@
+package Java_Videos;
+
+public class Car_32 {
+
+    String name;
+
+    Car_32(String name){
+
+        this.name = name;
+
+    }
+
+}
---
+++ src/Java_Videos/Garage_32.java
@@ -0,0 +1,9 @@
+package Java_Videos;
+
+public class Garage_32 {
+
+    void park(Car_32 car){
+        System.out.println("The "+car.name+" is parked in the garage");
+    }
+
+}
